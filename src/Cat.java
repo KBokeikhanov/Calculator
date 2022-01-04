d8514e4c5be0ecc35c4d6eab838be5d9749960e9
KBoke <KBokeikhanov@beeline.kz>
2022-01-04 11:21:36 +0600

    Итоговое задание по основам ООП

---
+++ src/Cat.java
@@ -0,0 +1,17 @@
+public class Cat {
+
+    private String name;
+    private int age;
+    private int weight;
+
+    public Cat() {
+    }
+
+    public Cat(String name, int age, int weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+}
+
+
